9eb992d0907fe76fb78e260dc2711b5aa2e1297f
jakobehmsen <jakobehmsen.github@gmail.com>
2016-01-09 21:12:16 +0100

    Added support for or patterns.

--- intellij/src/midmod/rules/patterns/Pattern.java
+++ intellij/src/midmod/rules/patterns/Pattern.java
@@ -1,5 +1,8 @@
 package midmod.rules.patterns;
 
+import midmod.pal.Consumable;
+import midmod.rules.EdgePattern;
+import midmod.rules.Environment;
 import midmod.rules.RuleMap;
 
 public interface Pattern extends Comparable<Pattern> {
@@ -38,9 +41,55 @@ public interface Pattern extends Comparable<Pattern> {
 
             @Override
             public RuleMap.Node findNode(RuleMap.Node node) {
+                class OrEdgePattern implements EdgePattern {
+                    RuleMap.Node contentNode = new RuleMap.Node();
+                    RuleMap.Node firstNode = self.findNode(contentNode);
+                    RuleMap.Node secondNode = other.findNode(contentNode);
+
+                    @Override
+                    public Pattern pattern() {
+                        return OrPattern.this;
+                    }
+
+                    @Override
+                    public RuleMap.Node matches(RuleMap.Node target, Consumable consumable, Environment captures) {
+                        /*consumable.mark();
+                        captures.mark();
+
+                        RuleMap.Node n = firstNode.match(consumable, captures);
+
+                        if(n != null) {
+                            consumable.commit();
+                            captures.commit();
+
+                            return target;
+                        }
+
+                        consumable.rollback();
+                        captures.rollback();
+
+                        n = secondNode.match(consumable, captures);
+
+                        return n != null ? target : null;*/
+
+                        RuleMap.Node n = contentNode.match(consumable, captures);
+                        return n != null ? target : null;
+                    }
+                    @Override
+                    public boolean equals(Object obj) {
+                        return obj instanceof OrEdgePattern &&
+                            this.contentNode.getEdge(this.firstNode).equals(((OrEdgePattern)obj).contentNode.getEdge(((OrEdgePattern)obj).firstNode)) &&
+                            this.contentNode.getEdge(this.secondNode).equals(((OrEdgePattern)obj).contentNode.getEdge(((OrEdgePattern)obj).secondNode));
+                    }
+                }
+
+                return node.byPattern(new OrEdgePattern());
+
+                /*
                 // TODO: Implement this!!!
                 // Should be a new embedded node, from which the two alternatives are added
                 return null;
+                */
             }
         }
 
