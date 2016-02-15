c08db67c60106f68433284645250d325347dde6c
jakobehmsen <jakobehmsen.github@gmail.com>
2016-02-15 20:46:11 +0100

    Started work on a new project on combining JS/Nashorn with visual interactive data flow programming.

---
+++ intellij/src/jsflow/Main.java
@@ -0,0 +1,129 @@
+package jsflow;
+
+import jdk.nashorn.api.scripting.NashornScriptEngine;
+import jdk.nashorn.api.scripting.ScriptObjectMirror;
+
+import javax.script.*;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class Main {
+    private static class JSComponent extends JComponent {
+        private ScriptObjectMirror bindings;
+
+        private JSComponent(ScriptObjectMirror bindings) {
+            this.bindings = bindings;
+        }
+
+        @Override
+        public void paint(Graphics g) {
+            bindings.callMember("paint", g);
+            //ScriptObjectMirror paintFunction = (ScriptObjectMirror) bindings.get("paint");
+
+            //g.setColor(Color.RED);
+            //paintFunction.call(bindings, g);
+
+            //g.fillRect(0, 0, getWidth(), getHeight());
+        }
+
+        @Override
+        public void setBackground(Color bg) {
+            super.setBackground(bg);
+            bindings.setMember("background", bg);
+        }
+
+        @Override
+        public void setLocation(int x, int y) {
+            super.setLocation(x, y);
+            bindings.setMember("x", x);
+            bindings.setMember("y", y);
+        }
+
+        @Override
+        public void setSize(int width, int height) {
+            super.setSize(width, height);
+            bindings.setMember("width", width);
+            bindings.setMember("height", height);
+        }
+    }
+
+    private static Point mouseClickPoint;
+
+    public static void main(String[] args) {
+        ScriptEngineManager engineManager = new ScriptEngineManager();
+        NashornScriptEngine engine = (NashornScriptEngine) engineManager.getEngineByName("nashorn");
+
+        JFrame frame = new JFrame("jsflow");
+        JPanel desktop = (JPanel) frame.getContentPane();
+        desktop.setLayout(null);
+
+        JPopupMenu contextMenu = new JPopupMenu();
+
+        desktop.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                mouseClickPoint = e.getPoint();
+
+                if(contextMenu.isPopupTrigger(e)) {
+                    contextMenu.show(e.getComponent(), e.getX(), e.getY());
+                }
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                if(contextMenu.isPopupTrigger(e)) {
+                    contextMenu.show(e.getComponent(), e.getX(), e.getY());
+                }
+            }
+        });
+
+        try {
+            engine.eval("base = {}");
+            engine.eval("base.paint = function(g) { g.setColor(this.background); g.fillRect(0, 0, this.width, this.height); }");
+        } catch (ScriptException e) {
+            e.printStackTrace();
+        }
+
+        SimpleBindings baseBindings = new SimpleBindings();
+        //engine.eval("function paint(g) { g.setColor(background); g.fillRect(0, 0, width, height); }");
+        //ScriptObjectMirror base = (ScriptObjectMirror)baseBindings.get("nashorn.global");
+
+        contextMenu.add(new AbstractAction("New") {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                SimpleBindings bindings = new SimpleBindings();
+
+                //bindings.put("base", base);
+
+                ScriptObjectMirror cellObject = null;
+
+                try {
+                    //engine.eval("function paint(g) { g.setColor(background); g.fillRect(0, 0, width, height); }", bindings);
+                    cellObject = (ScriptObjectMirror)engine.eval("var n = {}; Object.setPrototypeOf(n, base);  n;");
+                    //obj.toString();
+                } catch (ScriptException e1) {
+                    e1.printStackTrace();
+                }
+
+                //ScriptObjectMirror cellObject = (ScriptObjectMirror)bindings.get("nashorn.global");
+                //cellObject.setProto(base);
+                JSComponent cell = new JSComponent(cellObject);
+                cell.setLocation(mouseClickPoint);
+                cell.setSize(20, 20);
+                cell.setBackground(Color.BLUE);
+                desktop.add(cell);
+                /*desktop.repaint();
+                desktop.revalidate();*/
+            }
+        });
+        //desktop.setComponentPopupMenu(contextMenu);
+
+        frame.setSize(800, 600);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
