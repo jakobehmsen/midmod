3b6424cb96d6aca1f768905aa84b4f9128aaa7af
jakobehmsen <jakobehmsen.github@gmail.com>
2016-06-08 13:50:49 +0200

    Added support for moving top-level views.

--- intellij/src/paidia/PlaygroundView.java
+++ intellij/src/paidia/PlaygroundView.java
@@ -1,11 +1,20 @@
 package paidia;
 
 import javax.swing.*;
+import javax.swing.border.AbstractBorder;
+import javax.swing.border.Border;
+import javax.swing.border.EtchedBorder;
 import javax.swing.text.JTextComponent;
 import java.awt.*;
 import java.awt.event.*;
+import java.awt.geom.Area;
+import java.awt.geom.GeneralPath;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 public class PlaygroundView extends JPanel {
     private EditableView currentEditableView;
@@ -169,5 +178,145 @@ public class PlaygroundView extends JPanel {
                 }
             }
         });
+
+        MouseAdapter mouseAdapter = new MouseAdapter() {
+            private JComponent selection;
+            private JComponent targetValueView;
+            private int mousePressX;
+            private int mousePressY;
+            private boolean linking;
+            private boolean moving;
+            private boolean hasDragged;
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+                if(e.getButton() == MouseEvent.BUTTON3) {
+                    if(valueView.getParent() != PlaygroundView.this)
+                        return;
+
+                    linking = true;
+                    int cursorType = Cursor.HAND_CURSOR;
+                    Component glassPane = ((RootPaneContainer)getTopLevelAncestor()).getGlassPane();
+                    glassPane.setCursor(Cursor.getPredefinedCursor(cursorType));
+                    glassPane.setVisible(cursorType != Cursor.DEFAULT_CURSOR);
+
+                    selection = new JPanel();
+                    selection.setBorder(BorderFactory.createDashedBorder(Color.BLACK));
+                    Point point = SwingUtilities.convertPoint(valueView.getParent(), valueView.getLocation(), PlaygroundView.this);
+                    selection.setSize(valueView.getSize());
+                    selection.setLocation(point);
+                    selection.setOpaque(false);
+                    add(selection, 0);
+                    selection.repaint();
+                    selection.revalidate();
+                } else if(e.getButton() == MouseEvent.BUTTON1) {
+                    targetValueView = Stream.iterate(valueView, c -> (JComponent)c.getParent()).filter(x -> x.getParent() == PlaygroundView.this).findFirst().get();
+
+                    //if(valueView.getParent() != PlaygroundView.this)
+                    //    return;
+
+                    moving = true;
+                    targetValueView.getParent().setComponentZOrder(targetValueView, 0);
+                    /*int cursorType = Cursor.MOVE_CURSOR;
+                    Component glassPane = ((RootPaneContainer)getTopLevelAncestor()).getGlassPane();
+                    glassPane.setCursor(Cursor.getPredefinedCursor(cursorType));
+                    glassPane.setVisible(cursorType != Cursor.DEFAULT_CURSOR);*/
+
+                    mousePressX = e.getX();
+                    mousePressY = e.getY();
+                }
+            }
+
+            @Override
+            public void mouseDragged(MouseEvent e) {
+                if(linking) {
+
+                } else if(moving) {
+                    if(!hasDragged) {
+                        int cursorType = Cursor.MOVE_CURSOR;
+                        Component glassPane = ((RootPaneContainer)getTopLevelAncestor()).getGlassPane();
+                        glassPane.setCursor(Cursor.getPredefinedCursor(cursorType));
+                        glassPane.setVisible(cursorType != Cursor.DEFAULT_CURSOR);
+                    }
+
+                    int deltaX = e.getX() - mousePressX;
+                    int deltaY = e.getY() - mousePressY;
+
+                    targetValueView.setLocation(targetValueView.getX() + deltaX, targetValueView.getY() + deltaY);
+                }
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                hasDragged = false;
+
+                if(e.getButton() == MouseEvent.BUTTON3 && linking) {
+                    /*linking = false;
+                    int cursorType = Cursor.DEFAULT_CURSOR;
+                    Component glassPane = ((RootPaneContainer)getTopLevelAncestor()).getGlassPane();
+                    glassPane.setCursor(Cursor.getPredefinedCursor(cursorType));
+                    glassPane.setVisible(cursorType != Cursor.DEFAULT_CURSOR);
+
+                    remove(selection);
+                    repaint(selection.getBounds());
+                    revalidate();
+
+                    Point pointInContentPane = SwingUtilities.convertPoint(valueView, e.getPoint(), PlaygroundView.this);
+                    ViewBinding targetView = findView(pointInContentPane);
+                    JComponent targetComponent = targetView.getView();//(JComponent) contentPane.findComponentAt(pointInContentPane);
+                    Point pointInTargetComponent = SwingUtilities.convertPoint(PlaygroundView.this, pointInContentPane, targetComponent);
+                    if(targetComponent != valueView) {
+                        // Target must support dumping a value on it
+                        Value projection = new Reduction(value.get());
+
+                        //Value projection = value.get().createProjection();
+                        ViewBinding projectionView = projection.toComponent();
+                        projectionView.setupWorkspace(workspace);
+                        projectionView.getView().setLocation(pointInTargetComponent);
+                        targetView.drop(projection, pointInTargetComponent);
+                        //targetComponent.add(projectionView.getView());
+
+                        projection.addUsage(new Usage() {
+                            ViewBinding origProjectionView = projectionView;
+
+                            @Override
+                            public void removeValue() {
+
+                            }
+
+                            @Override
+                            public void replaceValue(Value value) {
+                                if(origProjectionView.isCompatibleWith(value)) {
+                                    origProjectionView.updateFrom(value);
+                                    return;
+                                }
+
+                                ViewBinding projectionView = value.toComponent();
+                                projectionView.setupWorkspace(workspace);
+                                Point location = origProjectionView.getView().getLocation();
+
+                                origProjectionView.drop(value, location);
+
+                                //targetComponent.remove(origProjectionView.getView());
+                                projectionView.getView().setLocation(location);
+                                //targetComponent.add(projectionView.getView());
+                                //targetView.drop(projection);
+                                origProjectionView = projectionView;
+                            }
+                        });
+                    }*/
+
+                } else if(e.getButton() == MouseEvent.BUTTON1 && moving) {
+                    moving = false;
+                    int cursorType = Cursor.DEFAULT_CURSOR;
+                    Component glassPane = ((RootPaneContainer)getTopLevelAncestor()).getGlassPane();
+                    glassPane.setCursor(Cursor.getPredefinedCursor(cursorType));
+                    glassPane.setVisible(cursorType != Cursor.DEFAULT_CURSOR);
+                }
+            }
+        };
+
+        valueView.addMouseListener(mouseAdapter);
+        valueView.addMouseMotionListener(mouseAdapter);
     }
 }
